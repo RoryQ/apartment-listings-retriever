e328e8e9cb0d2d099f19cb864557fb47a657ea22
Rory Quinn <rory@getswift.co>
2019-02-24 21:01:11 +1100

    parse search page details

--- search.js
+++ search.js
@@ -1,9 +1,21 @@
+require("dotenv").config();
+
 const Axios = require("axios");
 const R = require("ramda");
 const TaskQueue = require("cwait").TaskQueue;
+const xpath = require("xpath");
+const DOMParser = require("xmldom").DOMParser;
+const parser = new DOMParser();
+
+const mariadb = require("mariadb");
+const conn = mariadb.createConnection(process.env.CONN_STRING);
 
 const search_url = "https://www.realestate.com.au/rent/property-house-unit+apartment-townhouse-between-0-500-in-melbourne,+vic+3000;+prahran,+vic+3181;+albert+park,+vic+3206;+brighton,+vic+3186;+armadale,+vic+3143/list-1?activeSort=list-date";
-const article = /<article class.*?id='(?<id>\w+)'/g;
+
+const regexes = {
+    article_id: /<article class.*?id='(?<id>\w+)'/g,
+    article: /(<article.*?<\/article>)/gs
+}
 
 function getMatches(string, regex, index) {
       index || (index = 1); // default to the first capturing group
@@ -15,11 +27,28 @@ function getMatches(string, regex, index) {
       return matches;
 }
 
-const formatListingUrl = (article_dom_id) => "https://www.realestate.com.au/" + article_dom_id.replace('t', '');
+const getId = (article) => xpath.select1('//@id', article).value.replace("t", "");
+const getAddress = (article) => xpath.select1('//a[@class="name"]/text()', article).data;
+const getPrice = (article) => xpath.select1('//p[@class="priceText"]/text()', article).data;
+const getDetails = (article) => R.pluck("data", xpath.select('//dl[contains(@class,"rui-property-features")]//text()', article));
 
-Axios.get(search_url)
-    .then(x => x.data)
-    .then(x => getMatches(x, article, 0))
-    .then(x => R.map(formatListingUrl, x))
-    .then(console.log);
+function constructRecord(article) {
+    return {
+        id: getId(article),
+        address: getAddress(article),
+        price: getPrice(article),
+        details: getDetails(article)
+    };
+}
+
+const tick = (async () => {
+    const details = await Axios.get(search_url)
+        .then(x => x.data)
+        .then(x => getMatches(x, regexes.article, 0))
+        .then(x => R.map(y => parser.parseFromString(y), x))
+        .then(x => R.map(y => constructRecord(y) ,x));
+
+    console.log(await details);
+});
+
 
